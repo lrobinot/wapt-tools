1c8b207fff94af3d08ad7a71fafe28e6c6ae82a4
Ludovic ROBINOT <robinot@geovariances.com>
2020-09-09 18:08:31 +0200

    Fix import

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/lrobinot/wapt-tools',
     author='Ludovic ROBINOT',
     author_email='lrobinot@gmail.com',
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     install_requires=['beautifulsoup4', 'requests'],
     version='0.3',
     license='Apache 2.0',
