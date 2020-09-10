ddd443b2306290a8396488bd5673c6e9d3d463c9
Ludovic ROBINOT <robinot@geovariances.com>
2020-09-10 17:16:15 +0200

    Save most of file contents to resources to avoid string transformation

--- wapttools/create.py
+++ wapttools/create.py
@@ -6,232 +6,7 @@ import gitlab
 import random
 from pkg_resources import resource_string
 
-data_vscode_extensions_json = '''{
-  "recommendations": [
-    "editorconfig.editorconfig",
-    "wayou.vscode-todo-highlight",
-    "ms-python.python"
-  ]
-}
-'''
-
-data_vscode_launch_json = '''{
-  "version": "0.2.0",
-  "configurations": [
-    {
-      "name": "WAPT: install",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "install",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal",
-      "linux": {
-        "sudo": true
-      },
-      "osx": {
-        "sudo": true
-      }
-    },
-    {
-      "name": "WAPT: remove",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "remove",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal",
-      "linux": {
-        "sudo": true
-      },
-      "osx": {
-        "sudo": true
-      }
-    },
-    {
-      "name": "WAPT: session-setup",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "session-setup",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal"
-    },
-    {
-      "name": "WAPT: -i build upload",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "-i",
-        "build-upload",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal",
-      "linux": {
-        "sudo": true
-      },
-      "osx": {
-        "sudo": true
-      }
-    },
-    {
-      "name": "WAPT: uninstall",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "uninstall",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal",
-      "linux": {
-        "sudo": true
-      },
-      "osx": {
-        "sudo": true
-      }
-    },
-    {
-      "name": "WAPT: audit",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "audit",
-        "-f",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal",
-      "linux": {
-        "sudo": true
-      },
-      "osx": {
-        "sudo": true
-      }
-    },
-    {
-      "name": "WAPT: update-package-sources",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "update-package-sources",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal",
-      "linux": {
-        "sudo": true
-      },
-      "osx": {
-        "sudo": true
-      }
-    },
-    {
-      "name": "WAPT: upgrade",
-      "type": "python",
-      "request": "launch",
-      "program": "${config:python.wapt-get}",
-      "args": [
-        "upgrade",
-        "-f",
-        "${workspaceFolder}"
-      ],
-      "console": "integratedTerminal",
-      "linux": {
-        "sudo": true
-      },
-      "osx": {
-        "sudo": true
-      }
-    }
-  ]
-}
-'''
-
-data_vscode_settings_json = '''{
-  "python.wapt-get": "${config:python.pythonPath}/../wapt-get.py",
-  "python.pythonPath": "C:\\Program Files (x86)\\wapt\\waptpython.exe",
-  "python.linting.enabled": false,
-  "python.linting.flake8Enabled": false,
-  "editor.rulers": [
-    120
-  ],
-  "todohighlight.include": [
-    "*.*"
-  ]
-}
-'''
-
-data_wapt_control = '''package           : geovar-{package}
-name              : ** enter name of the package **
-version           : 0.0.0-0
-architecture      : x64
-section           : base
-maintainer        : Geovariances IT Service (sysadmin@geovariances.com)
-description       :
-description_fr    :
-description_es    :
-description_pt    :
-depends           :
-conflicts         :
-maturity          : BETA
-locale            :
-target_os         : windows
-min_os_version    : 6.1
-min_wapt_version  : 1.8
-sources           : {url}/wapt/packages/{package}.git
-installed_size    :
-impacted_process  :
-audit_schedule    : 60
-editor            :
-licence           :
-keywords          :
-homepage          :
-changelog         :
-'''
-
-data_editorconfig = '''root = true
-
-[*]
-end_of_line = lf
-insert_final_newline = true
-charset = utf-8
-trim_trailing_whitespace = true
-indent_style = space
-indent_size = 2
-
-[*.ini]
-end_of_line = crlf
-
-[*.bat]
-end_of_line = crlf
-
-[*.py]
-indent_size = 4
-'''
-
-data_version_check = '''{
-  "url_version": "https://.../version-history",
-  "html": true,
-  "selector": "h6:nth-of-type(1)",
-  "cleaners": [
-    {
-      "pattern": "v([0-9.]*).*",
-      "rewrite": "\\1"
-    }
-  ],
-  "url_downloads": "https://.../builds",
-  "url_download_file": "https://download.../{version}.exe"
-}
-'''
-
+badge = '[![{text} status]({url}/wapt/packages/{package}/badges/{branch}/pipeline.svg?key_text={text})]({url}/wapt/packages/{package}/-/pipelines?&ref={branch}'
 
 def creator(package, verbose=False):
     gl = gitlab.Gitlab.from_config()
@@ -273,8 +48,8 @@ def creator(package, verbose=False):
     with open('README.md', 'w') as file:
         file.write('# WAPT {} package\n'.format(package_name))
         file.write('\n')
-        file.write('[![PROD status]({url}/wapt/packages/{package}/badges/master/pipeline.svg?key_text=PROD)]({url}/wapt/packages/{package}/-/pipelines?&ref=master)\n'.format(url=gl.url, package=package_name))
-        file.write('[![DEV status]({url}/wapt/packages/{package}/badges/develop/pipeline.svg?key_text=DEV)]({url}/wapt/packages/{package}/-/pipelines?&ref=develop)\n'.format(url=gl.url, package=package_name))
+        file.write(badge.format(text='PROD', branch='master', url=gl.url, package=package_name))
+        file.write(badge.format(text='DEV', branch='develop', url=gl.url, package=package_name))
 
     os.system('git add -A')
     os.system('git commit -m "Add README.md" README.md')
@@ -284,17 +59,17 @@ def creator(package, verbose=False):
 
     os.mkdir('.vscode')
     with open(os.path.join('.vscode', 'extensions.json'), 'w') as file:
-        file.write(data_vscode_extensions_json)
+        file.write(resource_string('wapttools.data', 'vscode_extensions.json'))
 
     with open(os.path.join('.vscode', 'launch.json'), 'w') as file:
-        file.write(data_vscode_launch_json)
+        file.write(resource_string('wapttools.data', 'vscode_launch.json'))
 
     with open(os.path.join('.vscode', 'settings.json'), 'w') as file:
-        file.write(data_vscode_settings_json)
+        file.write(resource_string('wapttools.data', 'vscode_settings.json'))
 
     os.mkdir('WAPT')
     with open(os.path.join('WAPT', 'control'), 'w') as file:
-        file.write(data_wapt_control.format(url=gl.url, package=package_name))
+        file.write(resource_string('wapttools.data', 'wapt_control').format(url=gl.url, package=package_name))
 
     os.mkdir('config')
     open(os.path.join('config', '.gitkeep'), 'a').close()
@@ -305,7 +80,7 @@ def creator(package, verbose=False):
         file.write('!.gitignore\n')
 
     with open('.editorconfig', 'w') as file:
-        file.write(data_editorconfig)
+        file.write(resource_string('wapttools.data', 'dot.editorconfig'))
 
     with open('.env', 'w') as file:
         file.write('PYTHONHOME=C:\\Program Files (x86)\\wapt\n')
@@ -324,7 +99,7 @@ def creator(package, verbose=False):
         file.write('    file: \'/ci/build.yml\'\n')
 
     with open('version-check.json', 'w') as file:
-        file.write(data_version_check)
+        file.write(resource_string('wapttools.data', 'version-check.json'))
 
     script = resource_string('wapttools.data', 'setup.tmpl')
     with open('setup.py', 'w') as file:
